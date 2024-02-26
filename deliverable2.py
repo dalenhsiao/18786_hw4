3ae9715ef7c642b03ee25d833c8f9cbb2eb3d8c2
dalenhsiao <dalenhsiao0523@gmail.com>
2024-02-26 02:25:22 -0500

    error in bleu

--- deliverable2.py
+++ deliverable2.py
@@ -10,25 +10,41 @@ def bleu_score(predicted: List[int], target: List[int], N: int) -> float:
     """
     if len(predicted) < N or len(target) < N:
         # TODO
-        pass
+        return 0 
     
     def C(y, g):
         # TODO how many times does n-gram g appear in y?
-        pass
+        # ***for each gram in n-grams, how many time the gram appear in sequence***
+        n = len(g)
+        count = 0 
+        left, right = 0, n
+        # sliding window
+        while right < len(y):
+            if g == tuple(y[left:right]):
+                count += 1
+            left += 1
+            right += 1
+        return count
+                        
 
     geo_mean = 1
-    for n in range(1, N+1):
+    for n in range(1, N+1): # for all N grams (e.g. N = 3, 4, 5, 6, ..., n)
         grams = set() # unique n-grams
+        Cs = []
         for i in range(len(predicted)-n+1):
             # TODO add to grams
-            pass
-        
-        numerator = None # TODO numerator of clipped precision
-        denominator = None # TODO denominator of clipped precision
+            pred_gram = tuple(predicted[i:i+n]) # extract n-gram from y_hat 
+            if pred_gram not in grams:
+                Cs.append(min(C(predicted, pred_gram), C(target, pred_gram))) # calculate C of each gram
+                grams.add(pred_gram) # check if gram is already there
+            
+            
+        numerator = np.sum(Cs) # TODO numerator of clipped precision
+        denominator = len(predicted) - n + 1 # TODO denominator of clipped precision
 
         geo_mean *= (numerator/denominator)**(1/N)
     
-    brevity_penalty = None # TODO
+    brevity_penalty = min(1, np.exp(1-len(target)/len(predicted))) # TODO
     return brevity_penalty * geo_mean
 
 
